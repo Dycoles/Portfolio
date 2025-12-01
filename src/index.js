fbdedee14b406f85b5ae35e94b3b7e48c7a9e724
Dycoles <colesdylan12@gmail.com>
2025-12-01 15:00:37 -0700

    finished project setup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 function HelloWorld() {
-  return <h1>Hello, World</h1>
+  return <h1 className="text-3xl font-bold underline"> Hello world! </h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
